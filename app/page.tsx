8cc3386c6d3d3a9dcef79d8ba2aa6a2619ff4d5e
liquid-dre <avdingiswayo54@gmail.com>
2026-03-11 20:27:30 +0200

    Remove why choose us section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from '@/components/Navbar'
 import HeroSection from '@/components/HeroSection'
 import AboutSection from '@/components/AboutSection'
 import ServicesSection from '@/components/ServicesSection'
-import WhyChooseUsSection from '@/components/WhyChooseUsSection'
 import GetStartedCTA from '@/components/GetStartedCTA'
 import TestimonialsSection from '@/components/TestimonialsSection'
 import Footer from '@/components/Footer'
@@ -14,7 +13,6 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <ServicesSection />
-      <WhyChooseUsSection />
       <GetStartedCTA />
       <TestimonialsSection />
       <Footer />
